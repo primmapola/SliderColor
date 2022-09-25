855b3050c4c370c1dba15e96988ac422f1c38dfc
don grigory <dingrygory29@gmail.com>
2022-09-25 23:49:05 +0300

    SetUp Project

--- SliderColor/ViewController.swift
+++ SliderColor/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewSquare: UIView!
+    
+    @IBOutlet weak var text1: UILabel!
+    @IBOutlet weak var text2: UILabel!
+    @IBOutlet weak var text3: UILabel!
+    
+  
+    @IBOutlet weak var slider1: UISlider!
+    @IBOutlet weak var slider2: UISlider!
+    @IBOutlet weak var slider3: UISlider!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewSquare.layer.cornerRadius = 30
+        
+        text1.text = String(slider1.value)
+        text1.textColor = UIColor.red
+
+        text2.text = String(slider2.value)
+        text2.textColor = UIColor.green
+
+        text3.text = String(slider3.value)
+        text3.textColor = UIColor.blue
     }
 
-
+    @IBAction func sliderOneChange(_ sender: UISlider) {
+        viewSquare.backgroundColor = UIColor(red: CGFloat(sender.value), green: CGFloat(slider2.value), blue: CGFloat(slider3.value), alpha: 1)
+        text1.text = String(round(sender.value * 100) / 100)
+    }
+    
+    @IBAction func sliderTwoChange(_ sender: UISlider) {
+        viewSquare.backgroundColor = UIColor(red: CGFloat(slider1.value), green: CGFloat(sender.value), blue: CGFloat(slider3.value), alpha: 1)
+        text2.text = String(round(sender.value * 100) / 100)
+    }
+    @IBAction func sliderThreeChange(_ sender: UISlider) {
+        viewSquare.backgroundColor = UIColor(red: CGFloat(slider1.value), green: CGFloat(slider2.value), blue: CGFloat(sender.value), alpha: 1)
+        text3.text = String(round(sender.value * 100) / 100)
+    }
 }
 
